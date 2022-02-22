f3fb0a42bd0066fcde4c835e6e745ffa2a36656a
scottcagno <scottiecagno@gmail.com>
2022-02-22 15:13:09 -0500

    [appstats] -- added a re-usable buffer and stats pool which seems to keep the http stats request overhead lower

--- pkg/appstats/appstats.go
+++ pkg/appstats/appstats.go
@@ -1,15 +1,14 @@
 package appstats
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"runtime"
-	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/cagnosolutions/appstats/pkg/appstats/internal/static"
@@ -46,11 +45,9 @@ func RegisterAppStats() {
 func Register(mux *http.ServeMux) {
 	mux.Handle(defaultRoot+"/", HandleIndex(defaultRoot))
 	mux.Handle(defaultRoot+"/stats", handleStats())
-	mux.Handle(defaultRoot+"/do/work", handleDoWork())
-	mux.Handle(defaultRoot+"/do/gc", handleGC())
 }
 
-type stats struct {
+type stat struct {
 	GoVersion    string           `json:"go_version"`
 	NumGoroutine int              `json:"num_goroutine"`
 	NumCPU       int              `json:"num_cpu"`
@@ -59,20 +56,65 @@ type stats struct {
 	LastGC       time.Time        `json:"last_gc"`
 }
 
+var stats *stat
+
+var statsPool = sync.Pool{
+	New: func() interface{} {
+		return &stat{
+			GoVersion:    runtime.Version(),
+			NumGoroutine: runtime.NumGoroutine(),
+			NumCPU:       runtime.NumCPU(),
+			CurrentTime:  time.Now(),
+		}
+	},
+}
+
+func getStats() *stat {
+	return statsPool.Get().(*stat)
+}
+
+func putStats(s *stat) {
+	statsPool.Put(s)
+}
+
+type buffer struct {
+	dat *bytes.Buffer
+	enc *json.Encoder
+}
+
+var buff *buffer
+
+var buffPool = sync.Pool{
+	New: func() interface{} {
+		b := &buffer{
+			dat: new(bytes.Buffer),
+		}
+		b.enc = json.NewEncoder(b.dat)
+		return b
+	},
+}
+
+func getBuffer() *buffer {
+	return buffPool.Get().(*buffer)
+}
+
+func putBuffer(b *buffer) {
+	b.dat.Reset()
+	buffPool.Put(b)
+}
+
 func writeStats(w io.Writer) error {
-	stats := stats{
-		GoVersion:    runtime.Version(),
-		NumGoroutine: runtime.NumGoroutine(),
-		NumCPU:       runtime.NumCPU(),
-		CurrentTime:  time.Now(),
-	}
+	stats = getStats()
+	defer putStats(stats)
 	runtime.ReadMemStats(&stats.Memory)
 	stats.LastGC = time.Unix(0, int64(stats.Memory.LastGC))
-	statsData, err := json.Marshal(stats)
+	buff = getBuffer()
+	defer putBuffer(buff)
+	err := buff.enc.Encode(stats)
 	if err != nil {
 		return err
 	}
-	_, err = w.Write(statsData)
+	_, err = buff.dat.WriteTo(w)
 	if err != nil {
 		return err
 	}
@@ -87,76 +129,6 @@ func handleStats() http.Handler {
 			http.Error(w, http.StatusText(code), code)
 			return
 		}
-	}
-	return http.HandlerFunc(fn)
-}
-
-func handleDoWork() http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		v, err := url.ParseQuery(r.URL.RawQuery)
-		if err != nil {
-			code := http.StatusExpectationFailed
-			http.Error(w, http.StatusText(code), code)
-			return
-		}
-		count := v.Get("count")
-		if count == "" {
-			code := http.StatusExpectationFailed
-			http.Error(w, "'id' was not found or was empty", code)
-			return
-		}
-		n, err := strconv.Atoi(count)
-		if err != nil {
-			code := http.StatusExpectationFailed
-			http.Error(w, "error converting id string to integer", code)
-			return
-		}
-		go doWork(n)
-		http.Redirect(w, r, defaultRoot+"/", http.StatusFound)
-	}
-	return http.HandlerFunc(fn)
-}
-
-func doWork(count int) {
-	m := make(map[int]interface{})
-	for i := 0; i < count; i++ {
-
-		var obj interface{}
-		switch i % 6 {
-		case 0:
-			obj = &struct {
-				_ uint32
-				_ uint16
-			}{}
-		case 1:
-			obj = &struct {
-				_ [3]uint64
-			}{}
-		case 2:
-			obj = fmt.Sprintf("a relatively long and useless string %d", i)
-		case 3:
-			obj = make([]byte, i%1024)
-		case 4:
-			obj = make([]byte, 10*i%1024)
-		case 5:
-			obj = make([]string, 512)
-		}
-
-		if i == 1000 {
-			m = make(map[int]interface{})
-			i = 0
-		}
-
-		m[i] = obj
-		time.Sleep(10 * time.Millisecond)
-	}
-	log.Printf("Worked with %d items. Done.\n", count)
-}
-
-func handleGC() http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		runtime.GC()
-		http.Redirect(w, r, defaultRoot+"/", http.StatusFound)
 	}
 	return http.HandlerFunc(fn)
 }
